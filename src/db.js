d6e1d40c0ad4bbdf3a7104c72390e276cba2aeac
Bruno mac <brunob43@outlook.com>
2023-02-03 14:08:22 -0300

    update

--- src/db.js
+++ src/db.js
@@ -39,6 +39,6 @@ Country.belongsToMany(Activity, { through : "activities_countries" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');;;
 };
 
